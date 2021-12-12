176f18e5f1b7818b32c5dcc01c41ea78bbd34344
gabriele-annese <gannese06@gmail.com>
2021-12-12 15:56:40 +0100

    maps left

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -9,7 +9,34 @@
 
         </div>
         <div class="info-ctn">
-
+            <div class="brand">
+                <img src="../assets/img/35.png" alt="">
+            </div>
+            <div class="info">
+                <h4>9840 Shore St. Found Du Lac, WI 54935</h4>
+                <p>0 1223 456 789</p>
+                <a href="/">mail@sofboz.com</a>
+            </div>
+            <div class="info-social">
+                <div v-for="(elSocial, i) in socialIcon" :key="`el-${i}`">
+                    <i :class="elSocial"></i>
+                </div>
+            </div>
+            <form class="message-ctn">
+                <div class="name-ctn">
+                    <input type="name" placeholder="Your Name" class="name">
+                    <input type="mail" placeholder="Your Email" class="mail">
+                </div>
+                <div class="message-area">
+                    <input type="text"  placeholder="Your Subject" class="text">
+                    <textarea name="messaggio" rows="5" cols="30" placeholder="Your Message"></textarea>
+                </div>
+                <div class="btn-ctn">
+                    <button class="btn">
+                        Send Message
+                    </button>
+                </div>
+            </form>
         </div>
       </section>
   </div>
@@ -17,6 +44,17 @@
 
 <script>
 export default {
+    name: 'Maps',
+    data(){
+        return{
+            socialIcon:[
+                'fab fa-twitter',
+                'fab fa-facebook-f',
+                'fab fa-instagram',
+                'fab fa-github'
+            ]
+        }
+    }
 
 }
 </script>
@@ -45,8 +83,89 @@ export default {
     }
     .info-ctn{
         width: 50%;
-        height: 300px;
-        background-color: red;
+        padding: 0 20px;
+        .info{
+            padding: 20px 0;
+            p{
+                color: #495057;
+                padding: 10px 5px;
+                cursor: pointer;
+                font-weight: 500;
+            }
+            a{
+                color: #495057;
+                text-decoration: none;
+                font-weight: 500;
+                &:hover,
+                &:focus{
+                    text-decoration: underline;
+                }
+            }
+        }
+    .info-social{
+        display: flex;
+        div{
+            width: 30px;
+            height: 30px;
+            border: 1px solid #f2f7ff;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 0 3px;
+            i{
+            cursor: pointer; 
+            padding: 0 10px;
+            color: #377dff;
+            }
+        }
+    }
+    .message-ctn{
+        display: flex;
+        flex-direction: column;
+        .name-ctn{
+            display: flex;
+            padding: 10px 0;
+                input{
+                  margin-right: 10px;
+                  outline: none;
+                  padding: 8px 5px;
+                  border: 1px solid #f2f7ff;
+                  border-radius: 5px;
+                &:focus{
+                    border: 1px solid #495057;
+                }
+                &::placeholder{
+                    color: #495057;
+                    font-weight: 500;
+                    font-size: 16px;
+                }
+                }
+        }
+        .message-area{
+            display: flex;
+            flex-direction: column;
+            input,
+            textarea{
+                margin-right: 10px;
+                outline: none;
+                padding: 8px 5px;
+                border: 1px solid #f2f7ff;
+                margin-bottom: 10px;
+                border-radius: 5px;
+                &:focus{
+                    border: 1px solid #495057;
+                }
+                &::placeholder{
+                    color: #495057;
+                    font-weight: 500;
+                    font-size: 16px;
+                }
+            }
+        }
+        .btn-ctn{
+            margin-top: 10px;
+        }
+    }
     }
 }
 </style>
